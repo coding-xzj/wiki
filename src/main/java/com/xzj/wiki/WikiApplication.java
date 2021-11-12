0f3f4b63f547972396951217ebfebf4e0e0a04f8
紫陌凛风 <63767972+zimo999mrx@users.noreply.github.com>
2021-11-12 20:29:30 +0800

    update 启动相关页面

--- src/main/java/com/xzj/wiki/WikiApplication.java
+++ src/main/java/com/xzj/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.xzj.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class WikiApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        ConfigurableApplicationContext app = SpringApplication.run(WikiApplication.class, args);
+        Environment env = app.getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址：\thttps://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
