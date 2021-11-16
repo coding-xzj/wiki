61a83694c5238143e83a9bec5fe103b5edb4f469
紫陌凛风 <63767972+zimo999mrx@users.noreply.github.com>
2021-11-16 14:07:00 +0800

    add ebook类

--- doc/wiki.sql
+++ doc/wiki.sql
@@ -26,19 +26,6 @@ values (4, 'Mysql 入门教程', '零基础入门 Mysql 开发，企业级应用
 insert into `ebook` (id, name, description)
 values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
 
-drop table if exists `test`;
-create table `test`
-(
-    `id`       bigint not null comment 'id',
-    `name`     varchar(50) comment '名称',
-    `password` varchar(50) comment '密码',
-    primary key (`id`)
-) engine = innodb
-  default charset = utf8mb4 comment ='测试';
-
-insert into `test` (id, name, password)
-values (1, '测试', 'password');
-
 -- 分类
 drop table if exists `category`;
 create table `category`
@@ -151,16 +138,3 @@ create table `ebook_snapshot`
     unique key `ebook_id_date_unique` (`ebook_id`, `date`)
 ) engine = innodb
   default charset = utf8mb4 comment ='电子书快照表';
-
-
-drop table if exists `demo`;
-create table `demo`
-(
-    `id`   bigint not null comment 'id',
-    `name` varchar(50) comment '名称',
-    primary key (`id`)
-) engine = innodb
-  default charset = utf8mb4 comment ='测试';
-
-insert into `demo` (id, name)
-values (1, '测试');
